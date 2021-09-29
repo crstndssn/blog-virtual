a22624ad9588e69c95fd81cb7f149cb4558afcc3
crstndssn <dussan29@gmail.com>
2021-09-28 22:39:14 -0500

    feature: add leyenda

--- src/views/fernando/Info.js
+++ src/views/fernando/Info.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="min-h-75 container mx-auto flex justify-center items-center flex-col text-justify">
-            <p className="md:text-2xl xs:text-xl my-5 md:w-2/4 xs:w-full">En<strong> Blog Virtual </strong> publicamos la reorganización empresarial admitida mediante Auto de Admisión No. 2020-01-617907 expedido por la Superintendencia de Sociedades, en virtud de lo señalado en el artículo 42 del Decreto 065 de 2020 y el Decreto 806 de 2020.</p>
+            <p className="md:text-2xl xs:text-xl my-5 md:w-2/4 xs:w-full">En<strong> Blog Virtual </strong> publicamos la reorganización empresarial admitida mediante Auto de Admisión No. 25-899-31-03-001-2020-00189-00 expedido por El Juzgado Primero Civil del Circuito de Zipaquirá, en virtud de lo señalado en el artículo 42 del Decreto 065 de 2020 y el Decreto 806 de 2020.</p>
         </div>
 
     )
